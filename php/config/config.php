31267c626176b108d0a1934e7f866914a5169e0f
BMC7 <bmcadena11@hotmail.com>
2017-06-04 20:35:17 -0500

    pagina al 87%

--- php/config/config.php
+++ php/config/config.php
@@ -1,5 +1,41 @@
 <?php 
+
+
 # Esta es la definicion de el nombre del host
 define('HOST', 'localhost');
+define('PASS', '');
+define('USER', 'root');
+
+
+$enlace = mysql_connect('HOST', 'USER', 'PASS');
+if (!$enlace) {
+    die('No pudo conectarse: ' . mysql_error());
+}
+
+$sql = 'CREATE DATABASE ilearning';
+if (mysql_query($sql, $enlace)) {
+    echo "La base de datos ilearning se creó correctamente\n";
+} else {
+    echo 'Error al crear la base de datos: ' . mysql_error() . "\n";
+}
+
+define('DB_NAME', 'ilearning');
+
+
+$sql = 'CREATE TABLE `ilearning`.`usuarios` ( `id_usuario` INT NOT NULL AUTO_INCREMENT , `nombre_usuario` TEXT NOT NULL , `correo_usuario` TEXT NOT NULL , `contrasenia_usuario` TEXT NOT NULL , PRIMARY KEY (`id_usuario`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_spanish_ci;';
+
+if (mysql_query($sql, $enlace)) {
+    echo "La tabla usuarios ilearning se creó correctamente\n";
+} else {
+    echo 'Error al crear la tabla usuarios: ' . mysql_error() . "\n";
+}
+
+##  Codigo para crear tabla usuarios
+/*
+
+CREATE TABLE `ilearning`.`usuarios` ( `id_usuario` INT NOT NULL AUTO_INCREMENT , `nombre_usuario` TEXT NOT NULL , `correo_usuario` TEXT NOT NULL , `contrasenia_usuario` TEXT NOT NULL , PRIMARY KEY (`id_usuario`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_spanish_ci;
+
+*/
+
 
  ?>
--- script/script_basico.js
+++ script/script_basico.js
@@ -1,4 +1,18 @@
- 	
+ function efectoTargeta(){
+
+ 			divEstructura = document.getElementById("Estructura");
+ 			divEstructura.style.backgroundColor = "#F5F5F5";
+ 			divEstructura.style.width = "85%";
+ }	
+
+ function efectoTargetaSalir(){
+
+ 			divEstructura = document.getElementById("Estructura");
+ 			divEstructura.style.backgroundColor = "#fff";
+ 			divEstructura.style.width = "80%";
+ }
+
+
  	var tamanoNavbar = document.getElementById("contenedor-barra-navegacion").offsetHeight;
 	// Obtine el tamaño de la ventana del usuario (navegador)
 	var altoVentanUsuario = (window.innerHeight);
@@ -6,3 +20,12 @@
  	var ventanPrimeraVista = document.getElementById("ventana-primera-vista");
     tamanototal = altoVentanUsuario - tamanoNavbar;
 	ventanPrimeraVista.style.height = tamanototal + "px";
+
+	var linkEstructura = document.getElementById("link-estructura");
+	linkEstructura.addEventListener("mouseover", efectoTargeta);
+	linkEstructura.addEventListener("mouseout", efectoTargetaSalir);
+
+
+
+
+
